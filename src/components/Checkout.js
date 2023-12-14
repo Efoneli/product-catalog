db3eb0674e2ffa6c5ea8bb9044bb1d0990595e55
Efoneli <abbaefoneli@gmail.com>
2023-12-14 08:54:15 +0100

    finsihing

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import thankyou from '../img/thankyou.gif'
+import { SignUpContext } from '../contexts/SignUpContext'
 
 const Checkout = () => {
+  const { loggedinUser } = useContext(SignUpContext)
   return (
-    <section className='flex flex-col justify-center items-center h-screen top-20 w-full text-center'>
+    <>
+      {
+        loggedinUser && <section className='flex flex-col justify-center items-center h-screen top-20 w-full text-center'>
         <img src={thankyou} alt='thankyou' className='h-[250px] my-3 md:h-[350px] mx-6' />
         <h2>See you again.</h2>
     </section>
+      }
+    </>
+    
   )
 }
 
